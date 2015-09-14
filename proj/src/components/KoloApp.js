13e7ad4c078d356f1148c88bb95587bcc33d98fd
enriched <feelingenriched@gmail.com>
2015-09-13 20:20:07 -0600

    Adding some preliminary svg logo stuff

--- proj/src/components/KoloApp.js
+++ proj/src/components/KoloApp.js
@@ -1,22 +1,24 @@
 'use strict';
 
 var React = require('react/addons');
-var ReactTransitionGroup = React.addons.TransitionGroup;
+var KoloLogo = require('./KoloLogo');
 
 // CSS
 require('normalize.css');
 require('../styles/main.scss');
 require('../styles/colors.scss');
 
-var imageURL = require('../images/kolologo.svg');
+//var imageURL = require('../images/kolologo.svg');
 
 var KoloApp = React.createClass({
   render: function() {
     return (
       <div className="main">
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-        </ReactTransitionGroup>
+        <KoloLogo />
+        <h1>KOLO UNGA</h1>
+        <h2>Header 2 Text</h2>
+        <h3>Header 3 Text</h3>
+        <div>Normal text.</div>
       </div>
     );
   }
---
+++ proj/src/components/KoloLogo.js
@@ -0,0 +1,15 @@
+var React = require('react/addons');
+
+var KoloLogo = React.createClass({
+
+  render: function() {
+    return (
+      <svg viewBox="0 0 100 11" fill="white">
+        <circle cx="6" cy="6" r="5"/>
+      </svg>
+    );
+  }
+
+});
+
+module.exports = KoloLogo;
