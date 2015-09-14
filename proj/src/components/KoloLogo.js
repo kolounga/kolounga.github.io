d4648f358e42d22591f2758362b6b505547df3e6
enriched <feelingenriched@gmail.com>
2015-09-14 17:57:24 -0600

    Making the logo actually draw correct

--- proj/src/components/KoloLogo.js
+++ proj/src/components/KoloLogo.js
@@ -1,17 +1,4 @@
 var React = require('react');
-var mat2d = require('gl-matrix').mat2d;
-var vec2 = require('gl-matrix').vec2;
-
-function generateCSSString(matrix) {
-  var str = '';
-  str += matrix[0].toFixed(5) + ',';
-  str += matrix[1].toFixed(5) + ',';
-  str += matrix[2].toFixed(5) + ',';
-  str += matrix[3].toFixed(5) + ',';
-  str += matrix[4].toFixed(5) + ',';
-  str += matrix[5].toFixed(5);
-  return 'matrix(' + str + ')';
-}
 
 const KoloLogo = React.createClass({
 
@@ -26,24 +13,19 @@ const KoloLogo = React.createClass({
     var barSep = scale * barSepRatio;
     var barLeft = right;
     var barRight = right + barSep;
-    var centerVec = vec2.fromValues((barLeft + barRight) / 2, barHeight / 2);
-
-    // Calculate some transforms for the bars
-    var leftBar = mat2d.fromTranslation(mat2d.create(), vec2.fromValues(barLeft, 0));
-    var rightBar = mat2d.fromTranslation(mat2d.create(), vec2.fromValues(barRight, 0));
-    var center = mat2d.fromTranslation(mat2d.create(), centerVec);
-    var backslash = mat2d.rotate(mat2d.create(), center, Math.PI / 4 );
-    var slash = mat2d.rotate(mat2d.create(), center, 3 * Math.PI / 4 );
-    mat2d.translate(backslash, backslash, vec2.fromValues(0, -centerVec[1]));
-    mat2d.translate(slash, slash, vec2.fromValues(0, -centerVec[1]));
+    var barCenter = (barLeft + barRight) / 2;
+    var centerx = barCenter + barWidth / 2;
+    var centery = barHeight / 2;
 
     // Generate the content
     return (
       <svg width="300" height="300" className="kolo-logo">
-        <rect width={barWidth} height={barHeight} transform={generateCSSString(leftBar)}/>
-        <rect width={barWidth} height={barHeight} transform={generateCSSString(rightBar)}/>
-        <rect width={barWidth} height={barHeight} transform={generateCSSString(backslash)}/>
-        <rect width={barWidth} height={barHeight} transform={generateCSSString(slash)}/>
+        <rect x={barLeft} width={barWidth} height={barHeight}/>
+        <rect x={barRight} width={barWidth} height={barHeight}/>
+        <rect x={barCenter} width={barWidth} height={barHeight}
+              transform={'rotate(45, ' + centerx + ',' + centery + ')'}/>
+        <rect x={barCenter} width={barWidth} height={barHeight}
+              transform={'rotate(-45,' + centerx + ',' + centery + ')'}/>
       </svg>
     );
   }
