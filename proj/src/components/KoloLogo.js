88980b5f0c8f248c8c16f724722d82464d7410a1
enriched <feelingenriched@gmail.com>
2015-09-14 22:08:10 -0600

    Still ge'en et

--- proj/src/components/KoloLogo.js
+++ proj/src/components/KoloLogo.js
@@ -15,35 +15,54 @@ const KoloLogo = React.createClass({
     this.setState({open: false});
   },
 
+  interpolateCord(start, end, perc) {
+    return [(end[0] - start[0]) * perc + start[0],
+            (end[1] - start[1]) * perc + start[1],
+            (end[2] - start[2]) * perc + start[2]];
+  },
 
   render: function() {
-    // Some characteristics
-    var scale = 15;
-    var right = 30;
-    var barRatio = 6.9;
-    var barSepRatio = 1.7;
-    var barWidth = scale;
-    var barHeight = scale * barRatio;
-    var barSep = scale * barSepRatio;
-    var barLeft = right;
-    var barRight = right + barSep;
-    var barCenter = (barLeft + barRight) / 2;
-    var centerx = barCenter + barWidth / 2;
-    var centery = barHeight / 2;
-
-    // Generate the content
     return (
       <Spring defaultValue={{val: 0}} endValue={{val: this.state.open ? 100 : 0}}>
         { i => {
+          var scale = 15;
+          var barRatio = 6.9;
+          var barSepRatio = 1.7;
+          var barSep = scale * barSepRatio;
+          var left = 30;
+          var right = left + barSep;
+          var center = left + barSep / 2;
+          var barWidth = scale;
+          var barHeight = scale * barRatio;
+          var halfWidth = barWidth / 2;
+          var halfHeight = barHeight / 2;
+          var menuSep = barWidth + 10;
+
+          var percent = i.val / 100;
+
+          var barBack = this.interpolateCord([center, 0, -45], [halfHeight, 0, 90], percent);
+          var centerBack = [barBack[0] + halfWidth, barBack[1] + halfHeight];
+
+          var barSlash = this.interpolateCord([center, 0, 45], [halfHeight, menuSep, 90], percent);
+          var centerSlash = [barSlash[0] + halfWidth, barSlash[1] + halfHeight];
+
+          var barRight = this.interpolateCord([right, 0, 0], [halfHeight, menuSep * 3, 90], percent);
+          var centerRight = [barRight[0] + halfWidth, barRight[1] + halfHeight];
+
+          var barLeft = this.interpolateCord([left, 0, 0], [halfHeight, menuSep * 2, 90], percent);
+          var centerLeft = [barLeft[0] + halfWidth, barLeft[1] + halfHeight];
+
           return (
-            <svg width="150" height="110" className="kolo-logo"
+            <svg width="150" height={110 + i.val} className="kolo-logo"
                  onMouseEnter={this.handleOpen} onMouseLeave={this.handleClose}>
-              <rect x={barLeft + i.val} width={barWidth} height={barHeight}/>
-              <rect x={barRight} width={barWidth} height={barHeight}/>
-              <rect x={barCenter} width={barWidth} height={barHeight}
-                    transform={'rotate(45, ' + centerx + ',' + centery + ')'}/>
-              <rect x={barCenter} width={barWidth} height={barHeight}
-                    transform={'rotate(-45,' + centerx + ',' + centery + ')'}/>
+              <rect x={barLeft[0]} y={barLeft[1]} width={barWidth} height={barHeight}
+                    transform={'rotate(' + barLeft[2] + ',' + centerLeft[0] + ',' + centerLeft[1] + ')'}/>
+              <rect x={barRight[0]} y={barRight[1]} width={barWidth} height={barHeight}
+                    transform={'rotate(' + barRight[2] + ',' + centerRight[0] + ',' + centerRight[1] + ')'}/>
+              <rect x={barSlash[0]} y={barSlash[1]} width={barWidth} height={barHeight}
+                    transform={'rotate(' + barSlash[2] + ',' + centerSlash[0] + ',' + centerSlash[1] + ')'}/>
+              <rect x={barBack[0]} y={barBack[1]} width={barWidth} height={barHeight}
+                    transform={'rotate(' + barBack[2] + ',' + centerBack[0] + ',' + centerBack[1] + ')'}/>
             </svg>
           );
         }}
